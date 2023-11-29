545035344bba1cf0cb897249278718ebd8552a9a
NAVYA AGARWAL <27agarwalnavya@gmail.com>
2023-11-29 21:00:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,189 @@
+import streamlit as st
+import preprocessor, helper
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import seaborn as sns
+
+st.sidebar.title("Social Media Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    mydata = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(mydata)
+
+    # fetch unique users
+    user_list = df['user'].unique().tolist()
+   # user_list.remove("group_notification")
+    user_list.sort()
+    user_list.insert(0, "Overall")
+
+    selected_user = st.sidebar.selectbox("Show Analysis with respect to", user_list)
+
+if st.sidebar.button("Show Analysis"):
+    st.title("Top Statistics")
+    num_messages, words, num_media_msg, links = helper.fetch_stats(selected_user, df)
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+        st.header("Total Messages")
+        st.title(num_messages)
+    with col2:
+        st.header("Total Words")
+        st.title(words)
+    with col3:
+        st.header("Media Shared")
+        st.title(num_media_msg)
+    with col4:
+        st.header("Links Shared")
+        st.title(links)
+
+    # monthly timeline
+    st.title("Monthly Timeline")
+    timeline = helper.monthly_timeline(selected_user, df)
+    fig, ax = plt.subplots()
+    ax.plot(timeline['time'], timeline['message'], color='purple')
+    plt.xticks(rotation='vertical')
+    st.pyplot(fig)
+
+    # daily timeline
+    st.title("Daily Timeline")
+    daily_timeline = helper.daily_timeline(selected_user, df)
+    fig, ax = plt.subplots()
+    ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='brown')
+    plt.xticks(rotation='vertical')
+    st.pyplot(fig)
+
+    #  Activity Map
+    st.title("Activity Map")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.header("Most Busy Day")
+        busy_day = helper.week_activity_map(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.bar(busy_day.index, busy_day.values, color='orange')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+    with col2:
+        st.header("Most Busy Month")
+        busy_month = helper.month_activity_map(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.bar(busy_month.index, busy_month.values, color='red')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+    # finding the busiest users in the group(Group Level)
+    if selected_user == 'Overall':
+        st.title('Most Busy Users')
+
+        x, new_df = helper.most_busy_user(df)
+
+        fig, ax = plt.subplots()
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            colors = cm.viridis(np.linspace(0, 1, len(x.index)))  # Create a colormap
+            ax.bar(x.index, x.values, color=colors)  # Use the colormap for the bars
+            plt.title('User Activity')  # Add a title
+            plt.xlabel('User')  # Add x-axis label
+            plt.ylabel('Activity Count')  # Add y-axis label
+            plt.xticks(rotation="vertical")
+            plt.grid(True)  # Add grid lines
+            st.pyplot(fig)
+
+        with col2:
+            st.write('## User Activity Data')  # Add a title to the DataFrame
+            st.dataframe(new_df.style.highlight_max(color='lightorange'))  # Highlight max values
+
+        # Weekly Activity Heatmap
+        st.title("Weekly Activity Map")
+        user_heatmap = helper.activity_heatmap(selected_user, df)
+        fig, ax = plt.subplots()
+
+        sns.heatmap(user_heatmap, cmap='viridis', linewidths=0.001, linecolor='white',
+                    cbar_kws={'label': 'Activity Level'}, ax=ax)
+        plt.xlabel('Hour of the Day', fontsize=10)
+        plt.ylabel('Day of the Week', fontsize=10)
+
+        st.pyplot(fig)
+
+    # WordCloud
+    st.title("Word Cloud for Frequent Words")
+    df_wc = helper.create_wordcloud(selected_user, df)
+    fig, ax = plt.subplots()
+    ax.imshow(df_wc)
+    st.pyplot(fig)
+
+    # Most Common words
+    most_common_df = helper.most_common_words(selected_user, df)
+
+    fig, ax = plt.subplots()
+
+    ax.barh(most_common_df["Words"], most_common_df["Frequency"])
+    plt.xticks(rotation='vertical')
+    st.title("Most Common Words")
+    st.pyplot(fig)
+
+    # User Sentiment Analysis
+    st.title("Sentiment Analysis")
+
+    # Perform sentiment analysis for the selected user or overall
+    user_sentiments = helper.analyze_sentiment(selected_user, df)
+
+    # If the selected user is 'Overall', display sentiment for all users
+    if selected_user == 'Overall':
+        overall_sentiment = {key: sum([user[key] for user in user_sentiments.values()]) / len(user_sentiments) for key
+                             in ['pos', 'neu', 'neg', 'compound']}
+        st.subheader("Overall")
+
+        # Create a horizontal bar chart for the overall sentiment scores
+        fig, ax = plt.subplots()
+        ax.barh(list(overall_sentiment.keys()), list(overall_sentiment.values()),
+                color=['green' if v >= 0 else 'red' for v in overall_sentiment.values()])
+        ax.set_xlabel('Score')
+        ax.set_title('Overall sentiment scores')
+        st.pyplot(fig)
+    else:
+        # If the selected user exists in the sentiment analysis results
+        if selected_user in user_sentiments:
+            sentiment_scores = user_sentiments[selected_user]
+
+            st.subheader(f"User: {selected_user}")
+
+            # Create a horizontal bar chart for the sentiment scores
+            fig, ax = plt.subplots()
+            ax.barh(list(sentiment_scores.keys()), list(sentiment_scores.values()),
+                    color=['green' if v >= 0 else 'red' for v in sentiment_scores.values()])
+            ax.set_xlabel('Score')
+            ax.set_title(f'Sentiment scores for {selected_user}')
+            st.pyplot(fig)
+        else:
+            st.write(f"No sentiment analysis results for {selected_user}")
+
+    ## Final results for the sentiment analysis
+    # Perform sentiment analysis for the selected user or overall
+    user_sentiments = helper.analyze_sentiment(selected_user, df)
+
+    # Display the sentiment score
+    sentiment = helper.sentiment_score(user_sentiments, selected_user)
+    st.subheader(f"The sentiment of {selected_user} is {sentiment}")
+
+    # Emoji Analysis
+    emoji_df = helper.emoji_helper(selected_user, df)
+    st.title("Emoji Analysis")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.dataframe(emoji_df)
+    with col2:
+        fig, ax = plt.subplots()
+
+        ax.pie(emoji_df["Frequency"].head(), labels=emoji_df["Emoji"].head(), autopct="%0.2f")
+        st.pyplot(fig)
---
+++ helper.py
@@ -0,0 +1,208 @@
+from urlextract import URLExtract
+from wordcloud import WordCloud
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+from collections import Counter
+import emoji
+
+extract = URLExtract()
+
+def fetch_stats(selected_user, df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    # Fetch the number of messages
+    num_messages = df.shape[0]
+
+    # Fetch the total number of words
+    words = []
+    for messages in df['message']:
+        words.extend(messages.split())
+
+    # Fetch number of media messages
+    num_media_msg = df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    # Fetch number of links shared
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+    return num_messages, len(words), num_media_msg, len(links)
+
+
+# finding the busiest users in the group(Group Level)
+def most_busy_user(df):
+    # Filter out 'group_notification'
+    df_filtered = df[df['user'] != 'group_notification']
+
+    x = df_filtered['user'].value_counts().head()
+
+    df_percent = round((df_filtered['user'].value_counts() / df_filtered.shape[0]) * 100, 2).reset_index().rename(
+        columns={'user': 'Name', 'count': 'Percent'})
+
+    return x, df_percent
+
+
+def create_wordcloud(selected_user, df):
+    f = open('stopwords.txt', 'r')
+    stop_words = f.read()
+
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    def remove_stop_words(message):
+        y = []
+        for word in message.lower().split():
+            if word not in stop_words:
+                y.append(word)
+        return " ".join(y)
+
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+    temp["message"] = temp["message"].apply(remove_stop_words)
+    df_wc = wc.generate(df['message'].str.cat(sep=" "))
+    return df_wc
+
+def most_common_words(selected_user, df):
+    f = open('stopwords.txt', 'r')
+    stop_words = f.read()
+
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    # Convert messages to lower case and strip white spaces
+    df['message'] = df['message'].str.lower().str.strip()
+
+    # Filters out '<media omitted>' and 'group_notification'
+    temp = df[
+        ~df['message'].isin(['<Media omitted>', 'group_notification', '<Media omitted>\n', '<Media', 'omitted>\n'])]
+
+    words = []
+    for message in temp['message']:
+        for word in message.split():
+            if word not in stop_words:
+                words.append(word)
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20)).rename(columns={0: "Words", 1: "Frequency"})
+
+    return most_common_df
+
+# Emoji Analysis
+def emoji_helper(selected_user, df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    emojis = []
+    for message in df['message']:
+        emojis.extend([c for c in message if emoji.demojize(c) != c])
+
+    emoji_df = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis)))).rename(
+        columns={0: "Emoji", 1: "Frequency"})
+    return emoji_df
+
+def monthly_timeline(selected_user, df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    timeline = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
+    time = []
+    for i in range(timeline.shape[0]):
+        time.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
+
+    timeline['time'] = time
+    return timeline
+
+def daily_timeline(selected_user, df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    daily_timeline = df.groupby('only_date').count()['message'].reset_index()
+
+    return daily_timeline
+
+def week_activity_map(selected_user, df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    return df['day_name'].value_counts()
+
+def month_activity_map(selected_user, df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    return df['month'].value_counts()
+
+def activity_heatmap(selected_user, df):
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    user_heatmap = df.pivot_table(index='day_name', columns='period', values='message', aggfunc='count').fillna(0)
+
+    return user_heatmap
+
+# Sentiment analysis of each user based on the text and emojis used
+nltk.download('vader_lexicon')
+
+def analyze_sentiment(selected_user, df):
+    sia = SentimentIntensityAnalyzer()
+    user_sentiments = {}
+
+    for index, row in df.iterrows():
+        user = row['user']
+        message = row['message']
+
+        if user not in user_sentiments:
+            user_sentiments[user] = {'compound': 0.0, 'pos': 0.0, 'neu': 0.0, 'neg': 0.0}
+
+        # Analyze text sentiment
+        text_sentiment = sia.polarity_scores(message)
+        for key in user_sentiments[user].keys():
+            user_sentiments[user][key] += text_sentiment[key]
+
+        # Analyze emoji sentiment
+        emojis = [c for c in message if emoji.demojize(c) != c]
+        for emo in emojis:
+            emoji_sentiment = {
+                '😀': 1.0,  # Positive sentiment
+                '😢': -1.0,  # Negative sentiment
+                '😠': -1.0,  # Negative sentiment
+                '😍': 1.0,  # Positive sentiment
+                '😐': 0.0,  # Neutral sentiment
+            }
+            if emo in emoji_sentiment:
+                user_sentiments[user]['compound'] += emoji_sentiment[emo]
+
+    # Average the sentiment scores
+    for user, sentiment_scores in user_sentiments.items():
+        total_messages = df[df['user'] == user].shape[0]
+        for key in sentiment_scores.keys():
+            sentiment_scores[key] /= total_messages
+
+    if selected_user == 'Overall':
+        return user_sentiments
+    else:
+        return {
+            selected_user: user_sentiments.get(selected_user, {'compound': 0.0, 'pos': 0.0, 'neu': 0.0, 'neg': 0.0})}
+
+def sentiment_score(user_sentiments, selected_user):
+    if selected_user == 'Overall':
+        pos = sum([user['pos'] for user in user_sentiments.values()]) / len(user_sentiments)
+        neg = sum([user['neg'] for user in user_sentiments.values()]) / len(user_sentiments)
+        neu = sum([user['neu'] for user in user_sentiments.values()]) / len(user_sentiments)
+    elif selected_user in user_sentiments:
+        sentiment_scores = user_sentiments[selected_user]
+        pos = sentiment_scores['pos']
+        neg = sentiment_scores['neg']
+        neu = sentiment_scores['neu']
+    else:
+        return "No sentiment analysis results for the selected user"
+
+    if (pos > neg) and (pos > neu):
+        return "Positive 😊"
+    elif (neg > pos) and (neg > neu):
+        return "Negative 😠"
+    else:
+        return "Neutral 🙂"
