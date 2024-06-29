fedd6a4f6ece9b85943bd9bcb4b0db68fc63e2d1
Navya Agarwal <123990921+NavyaAgarwal02@users.noreply.github.com>
2024-06-29 19:53:32 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import re
-
 def preprocess(data):
     pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
     messages = re.split(pattern, data)[1:]
-    dates = re.findall(pattern, data)
+    dates = re.findall(pattern,data)
 
-    df = pd.DataFrame({'User_message': messages, 'message_date': dates})
+    df = pd.DataFrame({'User_message' : messages , 'message_date': dates})
 
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %H:%M - ')
-    df.rename(columns={'message_date': 'date'}, inplace=True)
+    df['message_date'] = pd.to_datetime(df['message_date'], format = '%d/%m/%y, %H:%M - ')
+    df.rename(columns = {'message_date': 'date'}, inplace = True)
 
     users = []
     messages = []
@@ -24,24 +23,24 @@ def preprocess(data):
 
     df['user'] = users
     df['message'] = messages
-    df.drop(columns=['User_message'], inplace=True)
+    df.drop(columns = ['User_message'], inplace = True)
 
     df['year'] = df['date'].dt.year
     df['month'] = df['date'].dt.month_name()
-    df['hour'] = df['date'].dt.hour
+    df['hour']= df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
     df['month_num'] = df['date'].dt.month
     df['only_date'] = df['date'].dt.date
-    df['day_name'] = df['date'].dt.day_name()
+    df['day_name'] =df['date'].dt.day_name()
     period = []
     for hour in df[['day_name', 'hour']]['hour']:
         if hour == 23:
             period.append(str(hour) + "-" + str('00'))
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
-        else:
-            period.append(str(hour) + "-" + str(hour + 1))
+            period.append(str('00') + "-" + str(hour+1))
+        else: 
+            period.append(str(hour) + "-" + str(hour+1))
 
     df['period'] = period
-
+    
     return df
